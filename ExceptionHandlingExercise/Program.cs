3c1ca5916749f9dc358cdc2cb82a7db9e0fcc49a
robertsteele7 <atpcycle7@gmail.com>
2022-10-28 22:17:34 -0500

    completed

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.IO;
 
 namespace ExceptionHandlingExercise
@@ -17,21 +19,35 @@ namespace ExceptionHandlingExercise
             // and Exceptions will be thrown 
             // Below we will set this up 
             // ------------------------------------------------------------------------------
-
+            var arr = new char[] { '1', '2', '3', '4', '5', '6', 'a', 'b', 'c' };
+            var numbers = new List<int>();
+            var str = "";
 
 
             //TODO START HERE:
-            
+
             // Make a foreach loop to iterate through your character array
-            
+            foreach (var item in arr)
+            {
                 // Now create a try catch
-                
-                
-                    // Inside your try block
-                        // set your string variable to each array element in your char[] to .ToString()
-                        // Now, using int.Parse, parse your string variable and store in an int variable
-                        // Then add each int to your list
-                
+
+
+                // Inside your try block
+                // set your string variable to each array element in your char[] to .ToString()
+                // Now, using int.Parse, parse your string variable and store in an int variable
+                // Then add each int to your list
+                try
+                {
+                    str = item.ToString();
+                    var number = int.Parse(str);
+
+                    numbers.Add(number);
+
+                }catch(Exception e) 
+                {
+                    Console.WriteLine($"Unable to Parse '{item}'");
+                }
+            }
                 // catch your Exception:
                 // in the scope of your catch you can use the following, 
                 
@@ -39,10 +55,10 @@ namespace ExceptionHandlingExercise
                 
             
 
-            //foreach (var num in numbers)
-            //{
-            //    Console.WriteLine(num);
-            //}
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
